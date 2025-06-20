bc521715161a574e309b6f5179f35b8df40046a9
Eugene600 <ugowach160@gmail.com>
2025-06-20 14:35:10 +0300

    feat: Include an image carousel for displaying three images while adding some hover effects for call to action button in room display component

--- src/app/shared/components/room-display/room-display.ts
+++ src/app/shared/components/room-display/room-display.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { faLaptop, faUtensils, faSpa, faDumbbell, faHotTub, faConciergeBell, faVideo ,faPhone, faBath, faUserMd, faClock, faShieldAlt } from '@fortawesome/free-solid-svg-icons';
+import { Component, computed, signal } from '@angular/core';
+import { faLaptop, faUtensils, faSpa, faDumbbell, faHotTub, faConciergeBell, faVideo, faPhone, faBath, faUserMd, faClock, faShieldAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 
@@ -25,4 +25,36 @@ export class RoomDisplay {
     { icon: faClock, label: '24hr Service' },
     { icon: faShieldAlt, label: 'Security' },
   ];
+
+  // Signal holding the image list
+  readonly roomImages = signal<string[]>([
+    'images/future_classroom1.jpeg',
+    'images/future_classroom2.jpeg',
+    'images/laptop_with_software.jpeg',
+  ]);
+
+  // Signal to track the current image index
+  currentImageIndex = signal(0);
+
+  // Computed signal to return current image URL
+  currentImage = computed(() => this.roomImages()[this.currentImageIndex()]);
+
+  prevImage() {
+    const newIndex =
+      (this.currentImageIndex() - 1 + this.roomImages().length) %
+      this.roomImages().length;
+
+    this.currentImageIndex.set(newIndex);
+  }
+
+  nextImage() {
+    const newIndex =
+      (this.currentImageIndex() + 1) % this.roomImages().length;
+
+    this.currentImageIndex.set(newIndex);
+  }
+
+  goToImage(index: number) {
+    this.currentImageIndex.set(index);
+  }
 }
