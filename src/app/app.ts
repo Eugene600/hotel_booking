cfc6a392de21a7024a99d7715cbd9441f384f40e
Eugene600 <ugowach160@gmail.com>
2025-06-21 11:56:40 +0300

    feat: Added transition when navigating to login route

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { CoreModule } from './core/core-module';
-import {Footer} from "./core/layout/footer/footer";
+import { Footer } from "./core/layout/footer/footer";
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,7 @@ import {Footer} from "./core/layout/footer/footer";
   styleUrl: './app.css'
 })
 export class App {
-   constructor(public router: Router) {}
+  constructor(public router: Router) { }
 
   shouldShowHeader(): boolean {
     return this.router.url !== '/login';
--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faBed } from '@fortawesome/free-solid-svg-icons';
 
@@ -8,6 +8,13 @@ import { faBed } from '@fortawesome/free-solid-svg-icons';
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
-export class Login {
+export class Login implements OnInit {
   faBed = faBed;
+  animationClass = 'login-enter';
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.animationClass = 'login-enter login-enter-active';
+    }, 50);
+  }
 }
